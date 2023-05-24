5a9933067c8cc91f1ffc70c751226d8087e581e8
sirharis214 <hariskido214@gmail.com>
2023-05-24 10:39:53 -0400

    about.html layout

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require('php/client.php')
-
 ?>
 
 <html>
@@ -20,8 +19,7 @@ require('php/client.php')
   		<div class="col" align="center">
   			<div class="card p-3" style="width: 35rem;">
   				<form>
-    					<h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
+    				<h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 					<div class="form-floating py-2">
 						<input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
 						<label for="floatingInput">Email address</label>
@@ -30,7 +28,6 @@ require('php/client.php')
 						<input type="password" class="form-control" id="floatingPassword" placeholder="Password">
 						<label for="floatingPassword">Password</label>
 					</div>
-
 					<div class="checkbox mb-3">
 						<label>
 							<input type="checkbox" value="remember-me"> Remember me
